dad58e9c4a7e0353e4b5e9461efaa53eacbc6fa9
Marcelo Amaral Peres <amaralperesmarcelo@gmail.com>
2025-08-03 17:55:28 -0300

    basico+atualização das rotas

---
+++ API/src/server.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify'
+import {serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod"
+import { gamecontroller } from './routes/gamecontroler'
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(gamecontroller)
+
+app.listen({port: 3000}, () =>{
+    console.log('server rodando em http://localhost:3000')
+})
